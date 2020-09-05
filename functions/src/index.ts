49dd09c6b1f706bd5243dda34f2ff9a0a5ab54ba
alexlabbane <alex.labbane@tamu.edu>
2020-09-05 16:46:04 -0500

    test api get

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,11 @@
-//import * as functions from 'firebase-functions';
+const functions = require('firebase-functions');
+const cors = require('cors')({origin:true});
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+
+//Example POST vs GET
+let db = admin.firestore();
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -7,3 +14,15 @@
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+
+export const getAlerts = functions.https.onRequest((request: any, response: any) => {
+    cors(request, response, () => {
+        //let licensePlate = request.plateNumber;
+        let alertsRef = db.collection('alerts');
+        
+        alertsRef.get().then((alerts: any) => {
+            response.send(JSON.stringify(alerts));
+        });
+    });
+});
