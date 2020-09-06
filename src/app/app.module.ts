a42c277978814b757e458d612a1a770b07a989ef
Ian Matson <imatson9119@gmail.com>
2020-09-06 06:41:26 -0500

    Major frontend changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent, ImageDialog } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {MatIconModule} from '@angular/material/icon';
@@ -13,12 +13,18 @@ import {MatInputModule} from '@angular/material/input';
 import {MatExpansionModule} from '@angular/material/expansion';
 import { AutocompleteComponent } from './autocomplete/autocomplete.component';
 import { FormsModule } from '@angular/forms';
+import { OverlayscrollbarsModule } from 'overlayscrollbars-ngx';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatDialogModule} from '@angular/material/dialog';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AutocompleteComponent
+    AutocompleteComponent,
+    ImageDialog,
   ],
   imports: [
     BrowserModule,
@@ -30,6 +36,10 @@ import { FormsModule } from '@angular/forms';
     MatInputModule,
     MatExpansionModule,
     FormsModule,
+    OverlayscrollbarsModule,
+    MatTooltipModule,
+    MatProgressSpinnerModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
